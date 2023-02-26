2c982c6036e52f8e713bae76b0446785c59baac8
sudha-achanta <110560014+sudha-achanta@users.noreply.github.com>
2023-02-26 15:00:57 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -11,3 +11,29 @@ provider "aws" {
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
 }
+
+resource "aws_s3_bucket" "s3Bucket" {
+     bucket = "tf_state_02262023"
+     acl       = "public-read"
+
+     policy  = <<EOF
+{
+     "id" : "MakePublic",
+   "version" : "2012-10-17",
+   "statement" : [
+      {
+         "action" : [
+             "s3:GetObject"
+          ],
+         "effect" : "Allow",
+         "resource" : "arn:aws:s3:::[BUCKET_NAME_HERE]/*",
+         "principal" : "*"
+      }
+    ]
+  }
+EOF
+
+   website {
+       index_document = "index.html"
+   }
+}
