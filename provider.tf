804dc619c8642c30c1ea3d93304621a80769f843
sudha-achanta <110560014+sudha-achanta@users.noreply.github.com>
2023-02-26 10:06:34 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,1 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.56.0"
+    }
+  }
+}
 
+provider "aws" {
+  # Configuration options
+  region     = "us-west-2"
+  access_key = "my-access-key"
+  secret_key = "my-secret-key"
+}
